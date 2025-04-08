86f7d4091bd9c4eec29013f98fc1d1b8d652dcf0
숨 <stacycho0728@naver.com>
2025-04-08 16:35:04 +0900

    style : header 고정정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -7,11 +7,14 @@ import { Global } from "@emotion/react";
 import { globalStyles } from "styles/global";
 
 function App() {
+  const location = useLocation();
+  const isHome = location.pathname === "/home";
+
   return (
     <>
       <Global styles={globalStyles} />
       <Header />
-      <Outlet />
+      {!isHome && <div style={{ height: "65px" }} />} <Outlet />
       <Footer />
     </>
   );
