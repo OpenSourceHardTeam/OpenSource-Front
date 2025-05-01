ef42ed37065abc728e76aff33b7dff7e58ac947b
DongJae Kim <ehdwo1227@naver.com>
2025-05-01 20:56:37 +0900

    feat: 회원가입 UI 구현

---
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,37 @@
+import * as styles from "./SignUpPage.style";
+import Button from "@components/Button/Button";
+import { useNavigate } from "react-router-dom"
+
+const SignUpPage = () => {
+
+    const navigate = useNavigate();
+
+    const onClickBackToLogin = () => {
+        navigate("/login");
+    };
+
+  return (
+    <div css={styles.signupframe}>
+      <div css={styles.signuptext}>회원가입</div>
+      <div css={styles.signupcheck}>
+        <input css={styles.signupnickname} placeholder="닉네임" />
+        <Button text={"중복확인"} type={"duplicate"} />
+      </div>
+      <div css={styles.signupcheck}>
+        <input css={styles.signupid} placeholder="아이디" />
+        <Button text={"중복확인"} type={"duplicate"} />
+      </div>
+      <input css={styles.signuppw} placeholder="비밀번호" />
+      <input css={styles.signuppw} placeholder="비밀번호 확인" />
+      <div css={styles.checkboxContainer}>
+        <input type="checkbox" id="agree-checkbox" css={styles.signupcheckbox}/>
+        <label htmlFor="agree-checkbox" css={styles.checkboxLabel}>이용약관 및 개인정보처리방침에 동의합니다</label>
+      </div>
+      <Button text={"로그인"} type={"login"} />
+      <Button text={"가입하기"} type={"login"} />
+      <div css={styles.backtologintext} onClick={onClickBackToLogin}>로그인 화면으로 돌아가기</div>
+    </div>
+  );
+};
+
+export default SignUpPage;
